38eff1aea750bad401685172484b993f0e9b2c4f
Alexander Grajales <alexander.grajales@ilimitada.co>
2018-07-07 15:45:04 -0500

    cambios para que el inicio de sesion y registro funcione correctamente

--- empleados/view.py
+++ empleados/view.py
@@ -17,13 +17,13 @@ def usuarios():
 
 
 def usuario():
-    name = request.json["name"]
+    name = request.json["email"]
     password = request.json["password"]
     cursor = DATA_PROVIDER.conn.cursor()
     cursor.execute(generateSELECT(TABLE, None, True, " {0} {1} = '{2}' and {3} = '{4}'".format(WHERE, NOMBRE, name, CLAVE, password)))
     query_result = [dict(line) for line in
                     [zip([column[0] for column in cursor.description], row) for row in cursor.fetchall()]]
-    return json.dumps(query_result)
+    return json.dumps({'Respuesta': len(query_result) != 0})
 
 
 def usuarioscreate():
@@ -35,7 +35,7 @@ def usuarioscreate():
         cursor = DATA_PROVIDER.conn.cursor()
         cursor.execute(generateINSERT(TABLE, COLUMS, "({0}, '{1}', {2}, '{3}')".format(id, first_name, ccosto, clave)))
         cursor.commit()
-        return json.dumps({'Respuesta': 'True'})
+        return json.dumps({'uid': id})
     except Exception as excep:
         return json.dumps({'Respuesta': 'False'})
 
